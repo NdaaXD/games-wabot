a69949f42191653ae634187eb382672de7cd19f7
NdaaXD <ndaatzy123@gmail.com>
2022-02-21 01:44:14 +0700

    Wabot Games

---
+++ config.js
@@ -0,0 +1,68 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['', '']
+global.Owner = {
+  // use the phone number with the country code, for example Indonesia '62' America '1' etc
+  '6288239928939': { // put your number here
+    name: 'Ndaa',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: false, // if true this number will send if user use command /report or /join
+    isCreator: true // if true this number will send if user use command /owner or /creator
+  },
+  '6288239928939': { // put your number here
+    isDev: true, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6288239928939': { // put your number here
+    name: 'Ndaa',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6285713964963': {
+    isReport: true // if true this number will send if user use command /report or /join
+  },
+  '13479805233': {} // ignore if you don't want anything, like name dll
+}
+global.mods = ['13479805233'] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  hardianto: 'http://hardianto-chan.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'http://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = ''
+global.author = ''
+
+global.multiplier = 9 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
